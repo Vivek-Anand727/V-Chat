fba39124f41343fcd486317cabaf7b2abf3382ee
Vivek-Anand727 <kamjorkismatwala@gmail.com>
2025-06-29 20:07:19 +0530

    Deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://v-chat-backend-9jiw.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
